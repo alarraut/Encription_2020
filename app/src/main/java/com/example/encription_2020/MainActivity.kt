d3882debabfc43abdbf2e30b0aa352f50fc7ac51
alarraut <alarraut@hotmail.com>
2020-03-21 22:28:49 -0600

    Se depura el codigo de la aplicacion

--- app/src/main/java/com/example/encription_2020/MainActivity.kt
+++ app/src/main/java/com/example/encription_2020/MainActivity.kt
@@ -1,15 +1,10 @@
 package com.example.encription_2020
 
-import android.content.ClipData
-import android.content.ClipboardManager
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
+import android.view.View as View
 
 
 const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
@@ -23,17 +18,13 @@ class MainActivity : AppCompatActivity() {
 
     var anta: MyMethods? = null
 
-    fun codificar(view: View?) {
+    fun codificar(view: View) {
       anta = MyMethods()
         val texto: String
         val llave: String
         val mensajecod: String
-        var myClipboard: ClipboardManager? = null
-        var myClip: ClipData? = null
-       // val intent = Intent(this, DisplayMessageActivity::class.java)
 
         //Recepcion del texto y llave
-
         val editllave = findViewById<EditText>(R.id.editllave)
         val edittexto = findViewById<EditText>(R.id.edittexto)
         texto = edittexto.text.toString()
@@ -41,13 +32,6 @@ class MainActivity : AppCompatActivity() {
 
         // Envio de parametros para su codificacion
         mensajecod = anta!!.codifica(texto, llave)
-
-        // Se copia mensaje codificado al portapapeles
-        myClipboard = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager?;
-        myClip = ClipData.newPlainText("text", mensajecod);
-        myClipboard?.setPrimaryClip(myClip);
-
-        Toast.makeText(this, "Text Copied", Toast.LENGTH_SHORT).show();
 
         //Compartir mensaje
         val sendIntent: Intent = Intent().apply {
@@ -66,14 +50,12 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun decodificar(view: View?) {
+    fun decodificar(view: View) {
 
        anta = MyMethods()
         val texto: String
         val llave: String
         val mensajedecod: String
-        var myClipboard: ClipboardManager? = null
-        var myClip: ClipData? = null
 
         //Recepcion del texto y llave
         val editllave = findViewById<EditText>(R.id.editllave)
@@ -84,10 +66,7 @@ class MainActivity : AppCompatActivity() {
         // Envio de parametros para su decodificacion
         mensajedecod = anta!!.decodifica(texto, llave)
 
-        // Se copia mensaje codificado al portapapeles
-        myClipboard = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager?;
-        myClip = ClipData.newPlainText("text", mensajedecod);
-        myClipboard?.setPrimaryClip(myClip)
+
 
         //Share
         val sendIntent: Intent = Intent().apply {
@@ -106,7 +85,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-       fun compartir (view: View?){
+       fun compartir(view: View) {
         val sendIntent: Intent = Intent().apply {
             action = Intent.ACTION_SEND
             putExtra(Intent.EXTRA_TEXT, "This is my text to send.")
