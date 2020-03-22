76336d81fc832ad754b7afaaf0e4bb321141a526
alarraut <alarraut@hotmail.com>
2020-03-21 22:07:48 -0600

    Se agrega e boton de compartir para poder enviarlo por cualquier aplicacion

--- app/src/main/java/com/example/encription_2020/MainActivity.kt
+++ app/src/main/java/com/example/encription_2020/MainActivity.kt
@@ -90,7 +90,15 @@ class MainActivity : AppCompatActivity() {
 
 
 
+    fun compartir (view: View?){
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, "This is my text to send.")
+            type = "text/plain"
+        }
 
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
 
-
+    }
 }
